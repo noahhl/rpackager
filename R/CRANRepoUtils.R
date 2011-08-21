6d2b9dd5511820cba516d1b3f0546879f924b88f
Noah Lorang <noah@noahhl.com>
2011-08-21 12:11:08 -0400

    More progress on getting Packager.Update() to work

--- R/CRANRepoUtils.R
+++ R/CRANRepoUtils.R
@@ -1,7 +1,11 @@
 GetAvailablePackages <- function(repos) {
   availablePackages <- data.frame()
   for(i in 1:length(repos)) {
-    availablePackages <- rbind(availablePackages, data.frame(available.packages(contrib.url(repos[i])), stringsAsFactors=F))
+    a = suppressWarnings(available.packages(contrib.url(repos[i])))
+    if(nrow(a) == 0) {
+      a = suppressWarnings(available.packages(contrib.url(repos[i], type='source')))
+    }
+    availablePackages <- rbind(availablePackages, data.frame(a, stringsAsFactors=F))
   }
   availablePackages <- availablePackages[!duplicated(availablePackages$Package), ]
   return(availablePackages)
@@ -15,22 +19,32 @@ GetInstalledPackages <- function() {
 
 GetAvailableVersions <- function(package, repo = "http://cran.r-project.org") {
   current <- GetAvailablePackages(repo)
-  archive <- suppressWarnings(readLines(paste(repo, "/src/contrib/Archive/", package, sep="")))
-  archive <- archive[grep(".tar.gz", archive)]
-  versions <- gsub("<a href=|\"|.tar.gz|_","", substr(archive, regexpr("<a href=.*?.tar.gz", archive), regexpr("<a href=.*?.tar.gz", archive) + attr(regexpr("<a href=.*?.tar.gz", archive), "match.length")))
-  versions <- gsub(package, "", versions)
-  versions <- c(versions, current[current$Package == package, "Version"])
+  archive <- try(suppressWarnings(readLines(paste(repo, "/src/contrib/Archive/", package, sep=""))), silent=T)
+  if(length(archive) > 0) {
+    archive <- archive[grep(".tar.gz", archive)]
+    versions <- gsub("<a href=|\"|.tar.gz|_","", substr(archive, regexpr("<a href=.*?.tar.gz", archive), regexpr("<a href=.*?.tar.gz", archive) + attr(regexpr("<a href=.*?.tar.gz", archive), "match.length")))
+    versions <- gsub(package, "", versions)
+    versions <- c(versions, current[current$Package == package, "Version"])
+  } else {
+    versions = current[current[,"Package"] == package, "Version"]
+  }
   return(versions)
 }
 
 
-InstallVersion <- function(package, repo, version) {
+InstallCranVersion <- function(package, pkgversion, repo) {
   versions = GetAvailableVersions(package, repo)
-  
-  if(versions[length(versions)] == version) {
-    install.packages(package, repos = repo)
+  if(as.character(tryCatch(packageVersion(package), error=function(e){return("0.0.0")})) == gsub("-", ".", versions[length(versions)]) || (!is.na(pkgversion) && as.character(tryCatch(packageVersion(package), error=function(e) {return("0.0.0")})) == pkgversion)) {
+    return("Using")
   } else {
-    download.file(paste(repo, "/src/contrib/Archive/", package, "/", package, "_", version, ".tar.gz", sep=""), destfile = file.path(tempdir(), paste(package, ".tar.gz",sep="")))
-    install.packages(file.path(tempdir(), paste(package, ".tar.gz",sep="")), repos=NULL)
+    if(is.na(pkgversion) || versions[length(versions)] == pkgversion) {
+      v = tryCatch(install.packages(package, repos = repo), warning=function(w) {tryCatch(install.packages(package, repos=repo, type='source'), warning=function(w) {return("FAILED AT")})})
+      if(v == "FAILED AT")
+        return(v)
+    } else {
+      download.file(paste(repo, "/src/contrib/Archive/", package, "/", package, "_", pkgversion, ".tar.gz", sep=""), destfile = file.path(tempdir(), paste(package, ".tar.gz",sep="")))
+      install.packages(file.path(tempdir(), paste(package, ".tar.gz",sep="")), repos=NULL)
+    }
+    return("Installed")
   }
 }
--- R/Interface.R
+++ R/Interface.R
@@ -11,15 +11,16 @@ Packager.Require <- function(path = ".") {
 Packager.Install <- function(path = ".") {
   if(length(dir(path, pattern = "Packages.lock")) == 0) { #No lock file, so initial install from Package file -- same as a Packager.Update()
     Packager.Update(path)
+    LockPackages(file.path(path, "Packages")) 
   }
   else if(file.info("Packages")$mtime > file.info("Packages.lock")$mtime) { #Packages file has changed, need to check for difference with Packages.lock, resolve, and update lock file
     
   } else { #No update, just need to check for each package in Packages.lock and install needed ones
     lockedPackages <- yaml.load_file("Packages.lock")
     for(p in 1:length(lockedPackages$Dependencies)) {
-      version <- tryCatch(packageVersion(names(lockedPackages$Dependencies[p])), error=function(e) {InstallPackage(names(lockedPackages$Dependencies[p]), lockedPackages$Dependencies[[p]], lockedPackages$Sources)})
+      version <- tryCatch(packageVersion(names(lockedPackages$Dependencies[p])), error=function(e) {InstallPackage(names(lockedPackages$Dependencies[p]), lockedPackages$Dependencies[[p]], lockedPackages$pkgsources)})
       if(version != lockedPackages$Dependencies[[p]]) {
-        InstallPackage(names(lockedPackages$Dependencies[p]), lockedPackages$Dependencies[[p]], lockedPackages$Sources)
+        InstallPackage(names(lockedPackages$Dependencies[p]), lockedPackages$Dependencies[[p]], lockedPackages$pkgsources)
       }
     } 
   }
@@ -32,20 +33,20 @@ Packager.Install <- function(path = ".") {
 # single package to update as per the specifications in "Packages" and 
 # generate a new "Packages.lock" file.
 Packager.Update <- function(path = "./Packages", package = NULL) {
-  
+  Packages <- ReadPackageFile(path)
+  for(i in 1:nrow(Packages$packages)) {
+    pkgsource = ifelse(is.na(Packages$packages$source[i]), Packages$repos, Packages$packages$source[i])
+    InstallPackage(Packages$packages$name[i], Packages$packages$version[i], pkgsource)  
+  }
 }
 
 
-#
-Packager.Bundle <- function(path = "./Packages") {
-  
-}
-
-
-
-
-InstallPackage <- function(package, version, sources) {
-  cat("Package called: ", package)
-  cat("Version called: ", version)
-  cat("Sources called: ", sources)
+InstallPackage <- function(package, pkgversion, pkgsource) {
+  if(length(grep("git", pkgsource)) >0) {
+    GitInstall(gsub("git =>", "", pkgsource))
+    resp = "Installed"
+  } else if(length(grep("cran", pkgsource)) >0 || length(grep("=>", pkgsource)) == 0) {
+    resp <- InstallCranVersion(package, pkgversion, pkgsource)
+  }
+  cat(resp, package, "version", as.character(packageVersion(package)), "from", pkgsource, "\n")
 }
